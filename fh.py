7c9009f9f918189ad060838811d9346a82b1b355
Neil Bell <neilrbell@gmail.com>
2018-01-26 17:48:33 -0500

    moved executable code into the __main__ check so it isn't accidentally run outside

--- fh.py
+++ fh.py
@@ -2,15 +2,7 @@ import sys, random
 
 WORDLIST = "./words.txt" # 'c:\Python27\words.txt'
 
-#numWords  = int(raw_input("How many words to generate? "))
-#numRuns = raw_input("How many times to run? ")
-
-if len(sys.argv) < 2:
-	print "Usage: python.exe fh.py <#words to generate> <#times to run> > <file_out.ext>"
-
-
-numWords = int(sys.argv[1])
-numRuns = int(sys.argv[2])
+# This is where we'll put constants and "definitions" of things, not code that is explicitly going to execute.
 
 def textGen():
 	with open(WORDLIST, 'r') as in_file:
@@ -30,5 +22,20 @@ def multTextGen():
 
 		
 if "__main__" == __name__:
+	
+	# if you're running like a program, put all your "executable" code inside the __main__ check.
+	# This makes sure that other functions you define (like textGen or multTextGen) can be imported
+	# into other scripts without the "executable" stuff running. Like you wouldn't want the sys.argv check 
+	# breaking your other thing from running, because maybe all is good in _that_ context.
+	if len(sys.argv) < 2:
+		print "Usage: python.exe fh.py <#words to generate> <#times to run> > <file_out.ext>"
+	
+	# same thing as the sys.argv check, only check these here in the "executable" area. 
+	#numWords  = int(raw_input("How many words to generate? "))
+	#numRuns = raw_input("How many times to run? ")
+	numWords = int(sys.argv[1])
+	numRuns = int(sys.argv[2])
+	
+	
 	#textGen()
 	multTextGen()
